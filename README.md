00852ffb55ffa55528887150a1ff18f9ae372826
James Fu <jyfu77@gmail.com>
2025-11-17 22:22:14 -0800

    changes after getting new ubuntu system

---
+++ README.md
@@ -0,0 +1,10 @@
+This config assumes support of color in the terminal
+
+Put the following code in the beginning of your `~/.bashrc` file
+
+```bash
+for file in ~/.bashrc.d/*.sh;
+do
+source “$file”
+done
+```
--- misc.sh
+++ misc.sh
@@ -1,6 +1,46 @@
-source /usr/share/doc/git/contrib/completion/git-prompt.sh
+# colored prompt
 red=$(tput setaf 1)
 blue=$(tput setaf 4)
 cyan=$(tput setaf 6)
 reset=$(tput sgr0)
-PS1=':\[$blue\]\W \[$cyan\]$(__git_ps1 " (%s)")\n\[$red\]\$\[$reset\] '
+PS1='\[$blue\]\W \[$cyan\]$(__git_ps1 " (%s)")\n\[$red\]\$\[$reset\] '
+
+# colored GCC warnings and errors
+export GCC_COLORS='error=01;31:warning=01;35:note=01;36:caret=01;32:locus=01:quote=01'
+
+# don't put duplicate lines or lines starting with space in the history.
+# See bash(1) for more options
+HISTCONTROL=ignoreboth
+
+# append to the history file, don't overwrite it
+shopt -s histappend
+
+# for setting history length see HISTSIZE and HISTFILESIZE in bash(1)
+HISTSIZE=1000
+HISTFILESIZE=2000
+
+# check the window size after each command and, if necessary,
+# update the values of LINES and COLUMNS.
+shopt -s checkwinsize
+
+# If set, the pattern "**" used in a pathname expansion context will
+# match all files and zero or more directories and subdirectories.
+#shopt -s globstar
+
+# make less more friendly for non-text input files, see lesspipe(1)
+[ -x /usr/bin/lesspipe ] && eval "$(SHELL=/bin/sh lesspipe)"
+
+# enable programmable completion features (you don't need to enable
+# this, if it's already enabled in /etc/bash.bashrc and /etc/profile
+# sources /etc/bash.bashrc).
+if ! shopt -oq posix; then
+  if [ -f /usr/share/bash-completion/bash_completion ]; then
+    . /usr/share/bash-completion/bash_completion
+  elif [ -f /etc/bash_completion ]; then
+    . /etc/bash_completion
+  fi
+fi
+
+# for unity development on ubuntu
+export DOTNET_ROOT="$HOME/.dotnet"
+export PATH="$DOTNET_ROOT:$PATH"
